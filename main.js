11014550d9cd1302a50aa4b3aa64d35972fb6ff3
Bianca A <frank@Bianca-Avila-4.local>
2018-09-29 09:38:58 +0900

    delete button

--- main.js
+++ main.js
@@ -14,24 +14,28 @@ $(() => {
     function addValue() {
     	// display : flex for div that contains info
     	result.style.display = 'flex';
+    	// add the input value to the array
         toDo.push(input.value);
+        // create dom element 
 
-/i have problem that duplicates the values in the array /
+        // add id numebr that it was created here  as a class or name  attribute
         	let container = document.createElement('div');
         	container.setAttribute('class', 'task');
-        	console.log(container);
+        	// inner text 
         	let text = document.createElement('p').innerHTML= input.value;
         	result.append(container);
+        	// add text to the som 
         	container.append(text);
-
-        	console.log(toDo);
-
-        // toDo.map((e)=>{
-        	
-        // })
-
-        
+        	// delete button 
+        	let deleteButton= document.createElement('button')
+        	deleteButton.setAttribute('class', 'delete');
+        	container.append(deleteButton);
+    
     }
+
+    // add an event listener for x buttom to eliminate by ID of the array
+
+    //iterate through the div by query selector all to find the id  
 
 
 })
